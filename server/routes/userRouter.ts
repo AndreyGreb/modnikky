82637d510e857087ac33ec246d5d3e8419dfce4f
Andrey <andrey.grebeshov@gmail.com>
2021-05-06 16:18:20 +0300

    create controller from user registration

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -1,30 +1,9 @@
 import { Router } from 'express'
 
-const User = require('../models/User')
+const UserController = require('../controllers/userController')
 
 const router = Router()
 
-router.post('/registration', async (req, res) => {
-  try {
-    const { email, password } = req.body
-
-    const isUsed = await User.findOne({ email })
-
-    if (isUsed) {
-      return res.status(300).json({ message: 'Данный email уже занят!' })
-    }
-
-    const user = new User({
-      email,
-      password,
-    })
-
-    await user.save()
-
-    res.status(201).json({ message: 'Пользователь зарегистрирован!' })
-  } catch (error) {
-    console.log(error)
-  }
-})
+router.post('/registration', UserController.registration)
 
 module.exports = router
