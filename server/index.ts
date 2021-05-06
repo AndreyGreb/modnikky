f5d547ba765dfa525ae8c9a3dc67691013280a8f
Andrey <andrey.grebeshov@gmail.com>
2021-05-06 14:58:38 +0300

    подключение базы данных

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,26 @@
 import express from 'express'
+import mongoose from 'mongoose'
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
+const URI_DB= 'mongodb+srv://andrey:1q2w3e4r@cluster0.tqus9.mongodb.net/app?retryWrites=true&w=majority'
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`))
+async function start() {
+  try {
+      await mongoose.connect(URI_DB, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: true
+      })
+    app.listen(PORT, () =>
+      console.log(`Server has been started on port ${PORT}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
