0a9ff34143cd4899dcc0dc93262aabce9dcf33fc
Andrey <andrey.grebeshov@gmail.com>
2021-05-10 16:20:00 +0300

    настройка роутов

---
+++ src/routes.ts
@@ -0,0 +1,22 @@
+import Basket from './pages/Basket';
+import MainPage from './pages/MainPage';
+import NotFound from './pages/NotFound';
+import { MAIN_ROUTE, BASKET_ROUTE, NOT_FOUND_ROUTE } from './utils/consts';
+
+export const authRoutes = [
+  {
+    path: BASKET_ROUTE,
+    Component: Basket,
+  },
+];
+
+export const publicRoutes = [
+  {
+    path: MAIN_ROUTE,
+    Component: MainPage,
+  },
+  {
+    path: NOT_FOUND_ROUTE,
+    Component: NotFound,
+  },
+];
---
+++ src/utils/consts.ts
@@ -0,0 +1,5 @@
+export const MAIN_ROUTE = '/';
+export const LOGIN_ROUTE = '/login';
+export const REGISTRATION_ROUTE = '/registration';
+export const BASKET_ROUTE = '/basket';
+export const NOT_FOUND_ROUTE = '/error';
